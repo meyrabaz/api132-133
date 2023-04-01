dd5e821a14919880f4d73721ca900a5f241ee081
meyrabaz <meyrabaz@gmail.com>
2023-04-01 18:30:16 +0300

    01.04.2023.

---
+++ src/test/java/post_requests/Post03.java
@@ -0,0 +1,91 @@
+package post_requests;
+
+/* To create a POJO(Plain Old Java Object) Class do the followings
+       1)private variables
+       2)Constructor with all parameters
+       3)Constructor without parameter
+       4)Getters and Setters
+*/
+
+    /*
+     Given
+        https://jsonplaceholder.typicode.com/todos
+        {
+        "userId": 55,
+        "title": "Tidy your room",
+        "completed": false
+        }
+    When
+        I send POST Request to the Url
+    Then
+        Status code is 201
+    And
+        response body is like {
+                                "userId": 55,
+                                "title": "Tidy your room",
+                                "completed": false,
+                                "id": 201
+                                }
+ */
+
+
+
+
+
+
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+
+import static io.restassured.RestAssured.given;
+
+public class Post03 extends JsonPlaceHolderBaseUrl {
+    /*
+         Given
+            https://jsonplaceholder.typicode.com/todos
+            {
+            "userId": 55,
+            "title": "Tidy your room",
+            "completed": false
+            }
+        When
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
+
+    @Test
+    public void post03() {
+        //Set the url
+        spec.pathParam("first", "todos");
+
+        //Set the expected data
+        JsonPlaceHolderPojo expectedData = new JsonPlaceHolderPojo(33, "Tidy your room", false);
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request and get the response
+        Response response = given(spec).body(expectedData).post("{first}");
+        response.prettyPrint();
+
+        //Do assertion
+
+
+        
+
+
+    }
+
+}
+
+
+
+
+
