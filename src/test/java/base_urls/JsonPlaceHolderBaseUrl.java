48265338299ad23bb927e5673d00d56e6fc73129
meyrabaz <meyrabaz@gmail.com>
2023-04-06 18:31:14 +0300

    06.04.2023..

--- src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
+++ src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
@@ -1,31 +1,18 @@
 package base_urls;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
 public class JsonPlaceHolderBaseUrl {
 
+    protected RequestSpecification spec;
 
-      protected RequestSpecification spec;
+    @Before//This method will run before each @Test methods.
+    public void setUp() {
 
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://jsonplaceholder.typicode.com").build();
 
-     @Before
-      public void setUp(){
-
-       spec = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com/todos/23").build();
-
-
-
-
-
-
-      }
-
-
-
-
-
-
-
+    }
 }
--- src/test/java/get_requests/Get12.java
+++ src/test/java/get_requests/Get12.java
@@ -1,11 +1,15 @@
 package get_requests;
 
 import base_urls.GoRestBaseUrl;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertTrue;
 
 public class Get12 extends GoRestBaseUrl {
     /*
@@ -51,11 +55,40 @@ public class Get12 extends GoRestBaseUrl {
                         "data.name", hasItems("Marut Nambeesan DC", "Ms. Ankal Sinha", "Chaturbhuj Adiga"));
 
 
-    }
-}
+
      // I have list of data so i can calculate the number of Elements . Should be 10
 
-        // In assertion part there is not rule to assert we choose esay way or company says us which method they want.
+        // In assertion part there is not rule to assert we choose easy way or company says us which method they want.
+
+
+        // The female users are less than or equals to male users
+        //I will compare number of female and male users
+        //1st Way: I will get all genders in a list then calculate the number of females
+        JsonPath jsonPath = response.jsonPath();
+        List<String> allGenders = jsonPath.getList("data.gender");
+        System.out.println("allGenders = " + allGenders);//[female, female, male, female, male, female, male, male, male, male]
+
+        int numberOfFemales = 0;
+        for (String w : allGenders) {
+            if (w.equals("female")) {
+                numberOfFemales++;
+            }
+        }
+
+        System.out.println("numberOfFemales = " + numberOfFemales);
+        assertTrue(numberOfFemales <= allGenders.size() - numberOfFemales);
+
+        //2nd Way: I will get all females by using groovy language then compare numbers.
+        int numberOfFemaleUsers = jsonPath.getList("data.findAll{it.gender=='female'}").size();
+        System.out.println("numberOfFemaleUsers = " + numberOfFemaleUsers);//4
+
+        int numberOfMaleUsers = jsonPath.getList("data.findAll{it.gender=='male'}").size();
+        System.out.println("numberOfMaleUsers = " + numberOfMaleUsers);//6
+
+        assertTrue(numberOfFemaleUsers <= numberOfMaleUsers);
+
+    }
+}
 
 
 
