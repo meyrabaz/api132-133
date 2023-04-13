4e1f4edd750bd66e0c111b926692a7a1fc40c9b1
meyrabaz <meyrabaz@gmail.com>
2023-04-14 00:33:23 +0300

    14.04.2023

---
+++ src/test/java/herokuapp_smoketest/S4_Patch.java
@@ -0,0 +1,78 @@
+package herokuapp_smoketest;
+
+import base_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import utils.ObjectMapperUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static herokuapp_smoketest.S1_Post.bookingId;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class S4_Patch extends HerOkuAppBaseUrl {
+
+ /*
+    Given
+        https://restful-booker.herokuapp.com/booking/{id}
+    And
+          {
+            "firstname": "John",
+            "lastname": "Doe"
+          }
+    When
+        Send patch request
+
+    Then
+        Status code should be 200
+
+    And
+        {
+            "firstname": "John",
+            "lastname": "Doe",
+            "totalprice": 111,
+            "depositpaid": true,
+            "bookingdates": {
+                "checkin": "2023-01-01",
+                "checkout": "2024-01-01"
+            },
+            "additionalneeds": "Breakfast"
+        }
+
+     */
+
+
+    @Test
+
+    public void patchTest(){
+
+
+    // Set the Url
+       spec.pathParams("first","booking","second",bookingId);
+
+    // Set the expected data // Here we do not need to write all parameters others will be null
+        // but 2 parameters will change so we user map method and string
+
+        Map<String,String> expectedData = new HashMap<>();
+        expectedData.put("firstname","John");
+        expectedData.put("lastname","Doe");
+
+        //Send the request and get the response
+
+        Response response = given(spec).body(expectedData).patch( "{first}/{second}" );
+        response.prettyPrint();
+
+
+
+    // Do assertion (get actualData)
+        Map<String,String> actualData = ObjectMapperUtils.convertJsonToJavaObject(response.asString(),HashMap.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(200,response.statusCode());
+        assertEquals(expectedData.get("firstname"),actualData.get("firstname"));
+        assertEquals(expectedData.get("lastname"),actualData.get("lastname"));
+
+    }
+}
