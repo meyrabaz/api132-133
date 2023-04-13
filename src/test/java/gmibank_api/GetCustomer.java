578f3e0a0c492e226915a8d3a7ac6f4cc69fbd8b
meyrabaz <meyrabaz@gmail.com>
2023-04-13 18:21:29 +0300

    13.04.2023.

--- src/test/java/gmibank_api/GetCustomer.java
+++ src/test/java/gmibank_api/GetCustomer.java
@@ -1,6 +1,20 @@
 package gmibank_api;
 
-public class GetCustomer {
+import base_urls.GmiBankBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.Country;
+import pojos.Customer;
+import pojos.User;
+import utils.ObjectMapperUtils;
+
+import java.util.ArrayList;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static utils.AuthenticationGmiBank.gmiBankToken;
+
+public class GetCustomer extends GmiBankBaseUrl {
          /*
         Given
           https://www.gmibank.com/api/tp-customers/110452
@@ -45,5 +59,45 @@ public class GetCustomer {
       */
 
 
+    @Test
+    public void getCustomerById(){
+        //Set the url
+        spec.pathParams("first","api","second","tp-customers","third",110452);
+
+        //Set the expected data
+        Country country = new Country(3,"USA",null);
+        User user = new User(110016,"leopoldo.reinger","Jasmine","Stehr","marni.zboncak@yahoo.com",true,"en",null,null);
+        ArrayList<Object> accountList = new ArrayList<>();
+        Customer expectedData = new Customer(110452,"Jasmine","Stehr","V","marni.zboncak@yahoo.com","463-609-2097", "1-112-497-0270","16525","14387 Al Ridge5343 Bert Burgs","Waltermouth","761-59-2911","2021-11-28T21:00:00Z",false,country,"California",user,accountList);
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request and get the response
+        Response response = given(spec).
+                headers("Authorization","Bearer " + gmiBankToken()).
+                get("{first}/{second}/{third}");
+      //  response.prettyPrint();
+// MANUEL GET TOKEN : sign in gma bank > right click inspect clik on application session storage > httpsgmibank copy the token
+// OPEN DOC read how to get Token with automation. We create method for Token in UTILs
+
+        // Do assertion
+
+        Customer actualData = ObjectMapperUtils.convertJsonToJavaObject(response.asString(),Customer.class);
+       // System.out.println("actualData = " + actualData);
+
+       assertEquals(200,response.statusCode());
+       assertEquals(expectedData.getId(),actualData.getId());
+       assertEquals(expectedData.getFirstName(),actualData.getFirstName());
+        assertEquals(expectedData.getLastName(), actualData.getLastName());
+        //Rest is homework...
+       // try to do post request is our homework on lms
+
+
+    }
 }
+
+
+
+
+
+
 
---
+++ src/test/java/utils/AuthenticationGmiBank.java
@@ -0,0 +1,29 @@
+package utils;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthenticationGmiBank {
+
+    public static String gmiBankToken() {
+
+        Map<String, Object> postBody = new HashMap<>();
+        postBody.put("password", "Mark.123");
+        postBody.put("rememberMe", true);
+        postBody.put("username", "mark_twain");
+        Response response = given().body(postBody).contentType(ContentType.JSON).post("https://gmibank.com/api/authenticate");
+
+        return response.jsonPath().getString("id_token");
+    }
+
+}  // it will return Token but i just want to value of token
+       /* {
+            "id_token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJrX3R3YWluIiwiYXV0aCI6IlJPTEVfQURNSU4sUk9MRV9NQU5BR0VSIiwiZXhwIjoxNjgzOTkwNTE0fQ.6y_N938LP9xh6rJlD2JCEXsNuk4jp6QBWXHTuI9p2tIMMGBNJzZ_bQenaNcEq2fYVgwnBkd1o6WQDjiV0zVb-w"
+        }     not like this > just value
+        So we use jsonpath and string method */
+
