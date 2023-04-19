b98025aab8b9859c0f9a9dbf3627571ae86a31c6
meyrabaz <meyrabaz@gmail.com>
2023-04-19 12:49:47 +0300

    19.04.2023.

--- src/test/java/put_requests/Put03.java
+++ src/test/java/put_requests/Put03.java
@@ -1,4 +1,98 @@
 package put_requests;
 
-public class Put03 {
+import base_urls.DummyRestApiBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.DummyResponsePojo;
+import pojos.DummyRestApiPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Put03 extends DummyRestApiBaseUrl {
+    /*
+    Given
+           1) URL: https://dummy.restapiexample.com/api/v1/update/21
+           2)            {
+                            "employee_name": "Tom Hanks",
+                            "employee_salary": 111111,
+                            "employee_age": 23,
+                            "profile_image": "Perfect image"
+                         }
+
+     When
+
+        I send PUT Request to the Url
+
+     Then
+                i) Status code is 200
+
+     And
+
+                ii) Response body should be like the following
+                    {
+                        "status": "success",
+                        "data": {
+                            "employee_name": "Tom Hanks",
+                            "employee_salary": 111111,
+                            "employee_age": 23,
+                            "profile_image": "Perfect image"
+                        },
+                        "message": "Successfully! Record has been added."
+                    }
+
+     */
+
+    @Test
+    public void put02() {
+
+        //Set the url
+        spec.pathParams("first","update","second","21");
+
+        //Set the expected data
+
+        DummyRestApiPojo expectedData = new DummyRestApiPojo("Tom Hanks",111111,23,"Perfect image");
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request get the response
+        Response response = given(spec).body(expectedData).put("{first}/{second}");
+        response.prettyPrint();
+
+        //Do assertion
+        DummyResponsePojo actualData = response.as(DummyResponsePojo.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(200,response.statusCode());
+
+        assertEquals(expectedData.getEmployee_name(),actualData.getData().getEmployee_name());
+        assertEquals(expectedData.getEmployee_salary(),actualData.getData().getEmployee_salary());
+        assertEquals(expectedData.getEmployee_age(),actualData.getData().getEmployee_age());
+        assertEquals(expectedData.getProfile_image(),actualData.getData().getProfile_image());
+
+
+
+
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/put_requests/Put04.java
+++ src/test/java/put_requests/Put04.java
@@ -1,7 +1,46 @@
 package put_requests;
 
-public class Put04 {
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Put04 extends JsonPlaceHolderBaseUrl {
+
+      /*
+        Given
+            https://jsonplaceholder.typicode.com/todos/198
+        When
+            I send DELETE Request to the Url
+        Then
+            Status code is 200
+            And Response body is { }
+     */
+
+    @Test
+    public void put04(){
+
+        //Set the url
+        spec.pathParams("first","todos","second",198);
+
+        //Set the expected data
+        String expectedData = "{}";
+
+        //Send the request get the response
+        Response response = given(spec).delete("{first}/{second}");
+        response.prettyPrint();
+
+        //Do assertion
+//       JsonPlaceHolderPojo actualData = response.as(JsonPlaceHolderPojo.class);
+//        System.out.println("actualData = " + actualData);
+
+        assertEquals(200,response.statusCode());
+        assertEquals(expectedData, response.asString());
+
+
+    }
 
 
 }
